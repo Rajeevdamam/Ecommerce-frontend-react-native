a50ccd702cf7ae588f65378a9002506742afb018
Rajeev Damam <44741957+Rajeevdamam@users.noreply.github.com>
2021-11-28 22:12:39 +0530

    Delete env.ts

--- Constants/env.ts
+++
@@ -1,7 +0,0 @@
-export const MAPS_API_KEY = "AIzaSyAaFNvQihaGBcoPEuRXZ9mX6JC2BKoDkwI";
-export const IOS_LOGIN_API_KEY =
-	"312800137622-hpq5eoo4rq2a1omunhqbtqqgjjvs58k1.apps.googleusercontent.com";
-export const ANDROID_LOGIN_API_KEY =
-	"312800137622-kvc3lvb74b8d8b63f7sqgo0si4fsab2v.apps.googleusercontent.com";
-
-export const FB_ANDROID_LOGIN_KEY = "655497502274364";
